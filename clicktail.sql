a54205956d0d1319c3b66bff55cee24a5fa0b96e
dbaops <44490929+dbaops-com@users.noreply.github.com>
2020-04-10 17:36:27 +0800

    Update clicktail.sql

--- clicktail.sql
+++ clicktail.sql
@@ -1,4 +1,4 @@
-# 95line
+# 统计SQL的95线
 SELECT
     left(any(normalized_query),20),
     count(*) AS c,
@@ -11,3 +11,20 @@ GROUP BY normalized_query
 HAVING c > 1
 ORDER BY c DESC
 LIMIT 10;
+
+#
+# 统计全量SQL
+#
+SELECT
+    left(replaceRegexpOne(normalized_query, '[\,|\?| ]{6,}', ',?,?,?,?,?,?') ,100),
+    count(*) AS c,
+    max(query_time),
+    round(avg(query_time), 4) AS latency_avg,
+    round(quantile(0.95)(query_time), 4) AS latency_p95,
+    max(rows_examined),
+    round((latency_avg * c) / (max(_time) - min(_time)), 4) AS load
+FROM mysql_slow_log_orders
+WHERE normalized_query like '% tbname %'
+GROUP BY replaceRegexpOne(normalized_query, '[\,|\?| ]{6,}', ',?,?,?,?,?,?')
+ORDER BY replaceRegexpOne(normalized_query, '[\,|\?| ]{6,}', ',?,?,?,?,?,?') DESC
+LIMIT 1000;
