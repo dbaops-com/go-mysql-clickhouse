3a5ebedacf4c1c63cd89312e8a2626db032a305b
dbaops <44490929+dbaops-com@users.noreply.github.com>
2020-05-12 15:44:23 +0800

    Update configure.sql

--- configure.sql
+++ configure.sql
@@ -1,8 +1,8 @@
 #SQL1: 配置文件关键信息生成SQL
 SELECT 
     concat('SQLType,BinlogTime,ParseTime, ',group_concat(column_name ORDER BY ORDINAL_POSITION ASC)),
-    concat('?,?,?,',group_concat(if(length(column_name)>0, '?',''))),
-    CONCAT('0,0,1,',
+    concat('?,?,?,?,',group_concat(if(length(column_name)>0, '?',''))),
+    CONCAT('0,0,1,0,',
     GROUP_CONCAT(IF(column_type LIKE '%int%', 1, IF(column_type LIKE '%decimal%', 2, 0)) 
                  ORDER BY ORDINAL_POSITION ASC SEPARATOR ',' )) AS dataSQL, 
     CONCAT('CREATE TABLE IF NOT EXISTS ', table_name, '_shard ( ',
