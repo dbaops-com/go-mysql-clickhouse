0d3386dbda15402924cb2112bf2dbfed5025897b
dbaops <44490929+dbaops-com@users.noreply.github.com>
2020-06-24 10:20:14 +0800

    Update configure.sql

--- configure.sql
+++ configure.sql
@@ -1,29 +1,31 @@
 #SQL1: 配置文件关键信息生成SQL
+SET @db_name = 'schema_name';
+SET @tb_name = 'table_name';
+SET @dstname = 'dst_dbname.dst_tbname';
 SELECT 
-    concat('SQLType,BinlogTime,ParseTime,ServerId,BinlogXid,BinlogFile,BinlogPos, ',group_concat(column_name ORDER BY ORDINAL_POSITION ASC)),
-    concat('?,?,?,?,?,?,?,',group_concat(if(length(column_name)>0, '?',''))),
+    CONCAT('SQLType,BinlogTime,ParseTime,ServerId,BinlogXid,BinlogFile,BinlogPos,',group_concat(column_name ORDER BY ORDINAL_POSITION ASC)) AS columnList,
+    CONCAT('?,?,?,?,?,?,?,',group_concat(if(length(column_name)>0, '?',''))) AS posString,
     CONCAT('0,0,1,0,0,0,0,',
     GROUP_CONCAT(IF(column_type LIKE '%int%', 1, IF(column_type LIKE '%decimal%', 2, 0)) 
-                 ORDER BY ORDINAL_POSITION ASC SEPARATOR ',' )) AS dataSQL, 
-    CONCAT('CREATE TABLE IF NOT EXISTS ', table_name, '_shard ( ',
-    GROUP_CONCAT(CONCAT(column_name, ' ', 
-    IF(column_type LIKE '%int%','UInt64',
-    IF(column_type LIKE '%decimal%','String',
-    IF(column_type LIKE '%char%','String',''))))
-    ORDER BY ORDINAL_POSITION ASC SEPARATOR ','),
-    ', SQLType String, BinlogTime String, ServerId String, ParseTime UInt64, BinlogXid String, BinlogFile String, BinlogPos String) 
-    ENGINE = ReplicatedMergeTree(\'/clickhouse/tables/{layer}-{shard}/tttttt_replica\', \'{replica}\') 
-    PARTITION BY toDayOfMonth(toDate(uuuuuu / 1000)) ORDER BY (oooooo) SETTINGS index_granularity = 8192
-    ;') AS createSQL
+                 ORDER BY ORDINAL_POSITION ASC SEPARATOR ',' )) AS dataString, 
+    CONCAT('CREATE TABLE IF NOT EXISTS ', @dstname, ' ( ',
+        GROUP_CONCAT(CONCAT(column_name, ' ', 
+            IF(column_type LIKE '%int%',    'UInt64',
+            IF(column_type LIKE '%decimal%','String',
+            IF(column_type LIKE '%char%',   'String',''))))
+            ORDER BY ORDINAL_POSITION ASC SEPARATOR ','),
+        ', SQLType String, BinlogTime String, ServerId String, ParseTime UInt64, BinlogXid String, BinlogFile String, BinlogPos String)',
+        #'ENGINE = ReplicatedMergeTree(\'/clickhouse/tables/{layer}-{shard}/tttttt_replica\', \'{replica}\'',
+	    'ENGINE = MergeTree()',
+        'PARTITION BY toDayOfMonth(toDate(BinlogTime)) ORDER BY (BinlogTime) SETTINGS index_granularity = 8192;'
+	) AS createSQL INTO @columnList, @posString, @dataString, @createSQL
 FROM information_schema.columns 
-WHERE table_schema = 'ssssss' AND table_name = 'tttttt';
-
-需修改如下参数：
-ssssss：schema_name
-tttttt：table_name
-oooooo：orderby_column_list
-uuuuuu：Unixtime_column
-
+WHERE table_schema = @db_name AND table_name = @tb_name;
+SELECT CONCAT('DTKey=',@db_name,'.',@tb_name,'///INSERT INTO ',@dstname,'(',@columnList,') VALUES (', @posString,')')
+UNION ALL
+SELECT CONCAT('DTKey=',@db_name,'.',@tb_name,'-ColType///',@dataString)
+UNION ALL
+SELECT @createSQL;
 
 #SQL2: 查询最新数据
 SELECT 
