0632603492da0ce1951ac7cabab2edb420b2a6ea
dbaops <44490929+dbaops-com@users.noreply.github.com>
2020-06-24 09:05:00 +0800

    Update configure.sql
    修改配置生成SQL，增加BinlogFile和BinlogPos字段

--- configure.sql
+++ configure.sql
@@ -1,8 +1,8 @@
 #SQL1: 配置文件关键信息生成SQL
 SELECT 
-    concat('SQLType,BinlogTime,ParseTime,ServerId,BinlogXid, ',group_concat(column_name ORDER BY ORDINAL_POSITION ASC)),
-    concat('?,?,?,?,?,',group_concat(if(length(column_name)>0, '?',''))),
-    CONCAT('0,0,1,0,0,',
+    concat('SQLType,BinlogTime,ParseTime,ServerId,BinlogXid,BinlogFile,BinlogPos, ',group_concat(column_name ORDER BY ORDINAL_POSITION ASC)),
+    concat('?,?,?,?,?,?,?,',group_concat(if(length(column_name)>0, '?',''))),
+    CONCAT('0,0,1,0,0,0,0,',
     GROUP_CONCAT(IF(column_type LIKE '%int%', 1, IF(column_type LIKE '%decimal%', 2, 0)) 
                  ORDER BY ORDINAL_POSITION ASC SEPARATOR ',' )) AS dataSQL, 
     CONCAT('CREATE TABLE IF NOT EXISTS ', table_name, '_shard ( ',
@@ -11,7 +11,7 @@ SELECT
     IF(column_type LIKE '%decimal%','String',
     IF(column_type LIKE '%char%','String',''))))
     ORDER BY ORDINAL_POSITION ASC SEPARATOR ','),
-    ', SQLType String, BinlogTime String, ServerId String, ParseTime UInt64, BinlogXid String) 
+    ', SQLType String, BinlogTime String, ServerId String, ParseTime UInt64, BinlogXid String, BinlogFile String, BinlogPos String) 
     ENGINE = ReplicatedMergeTree(\'/clickhouse/tables/{layer}-{shard}/tttttt_replica\', \'{replica}\') 
     PARTITION BY toDayOfMonth(toDate(uuuuuu / 1000)) ORDER BY (oooooo) SETTINGS index_granularity = 8192
     ;') AS createSQL
